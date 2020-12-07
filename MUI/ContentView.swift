914c5bc757182cb754626e775f7f8f32202ac096
0xLeif <zmeriksen@icloud.com>
2020-12-07 16:38:29 -0600

    Initial Commit

---
+++ MUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MUI
+//
+//  Created by Zach Eriksen on 12/7/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MUI/MUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MUIApp.swift
+//  MUI
+//
+//  Created by Zach Eriksen on 12/7/20.
+//
+
+import SwiftUI
+
+@main
+struct MUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
